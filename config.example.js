48374edcf5c451943b4eb649d72a3fb39b13be8a
SkiF4er <cajeerskif4er@gmail.com>
2026-03-20 00:43:11 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   supabaseUrl: 'https://abcxyzcompany.supabase.co',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
+  supabaseAnonKey: 'ВАШ_СЕКРЕТНЫЙ_КЛЮЧ_БАЗЫ_ДАННЫХ',
   appTitle: 'Частотные отметки',
   defaultColor: '#D32F2F',
 };
