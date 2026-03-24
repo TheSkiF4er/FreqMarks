33c523bdb116dcea9b0751cdd24fde53c4b0983c
SkiF4er <cajeerskif4er@gmail.com>
2026-03-24 18:15:42 +0300

    Delete config.example.js

--- config.example.js
+++
@@ -1,6 +0,0 @@
-export const APP_CONFIG = {
-  supabaseUrl: 'https://abcxyzcompany.supabase.co',
-  supabaseAnonKey: 'ВАШ_СЕКРЕТНЫЙ_КЛЮЧ_БАЗЫ_ДАННЫХ',
-  appTitle: 'Частотные отметки',
-  defaultColor: '#D32F2F',
-};
